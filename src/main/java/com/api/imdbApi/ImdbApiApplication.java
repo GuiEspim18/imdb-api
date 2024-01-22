222b7794341b24ab498b6766374a1c81b2bb4b91
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-01-22 18:59:28 -0300

    add override run

--- src/main/java/com/api/imdbApi/ImdbApiApplication.java
+++ src/main/java/com/api/imdbApi/ImdbApiApplication.java
@@ -1,13 +1,19 @@
 package com.api.imdbApi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImdbApiApplication {
+public class ImdbApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImdbApiApplication.class, args);
+	}
+
+	@Override
+	public void run (String... args) {
+		System.out.println("Hello World!");
 	}
 
 }
