6a2e50ccb691230ebf9e8fad6f02c5b6c837f13b
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-01-22 18:55:01 -0300

    initialize

---
+++ src/test/java/com/api/imdbApi/ImdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.imdbApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
